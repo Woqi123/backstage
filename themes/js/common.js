3e9f32cd26196002b9e31cf232b287464e85bcc1
chenhao <wy.chenhao@qq.com>
2015-05-26 00:15:33 +0800

    1. 删除多余板块 2. iframe做内容主题区 3. 右上角问候angularjs输出

--- themes/js/common.js
+++ themes/js/common.js
@@ -1,36 +1,19 @@
-$(function() {
+//顶部条的问候
+angular.module('ngApp', []).controller('navTopGreet', function ($scope) {
+    $scope.hello = '晚上好';
+});
+$(function () {
 
+    //侧栏收缩
     $('#side-menu').metisMenu();
 
-});
-
-//Loads the correct sidebar on window load,
-//collapses the sidebar on window resize.
-// Sets the min-height of #page-wrapper to window size
-$(function() {
-    $(window).bind("load resize", function() {
-        topOffset = 50;
-        width = (this.window.innerWidth > 0) ? this.window.innerWidth : this.screen.width;
-        if (width < 768) {
-            $('div.navbar-collapse').addClass('collapse');
-            topOffset = 100; // 2-row-menu
-        } else {
-            $('div.navbar-collapse').removeClass('collapse');
-        }
-
-        height = ((this.window.innerHeight > 0) ? this.window.innerHeight : this.screen.height) - 1;
-        height = height - topOffset;
-        if (height < 1) height = 1;
-        if (height > topOffset) {
-            $("#page-wrapper").css("min-height", (height) + "px");
-        }
+    //展示区域iframe高度计算
+    $("#frameWrapper").load(function () {
+        var mainheight = $(this).contents().find("body").height();
+        $(this).height(mainheight);
     });
 
-    var url = window.location;
-    var element = $('ul.nav a').filter(function() {
-        return this.href == url || url.href.indexOf(this.href) == 0;
-    }).addClass('active').parent().parent().addClass('in').parent();
-    if (element.is('li')) {
-        element.addClass('active');
-    }
+    //title提示框美化
+    $('[data-toggle="tooltip"]').tooltip();
+
 });
