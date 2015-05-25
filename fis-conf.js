4284297b003aef2ee92247807c1b9341959b0c1b
chenhao <wy.chenhao@qq.com>
2015-05-25 14:52:50 +0800

    修改结构

--- fis-conf.js
+++ fis-conf.js
@@ -6,24 +6,6 @@ fis.config.merge({
         domain : {
             //所有css文件添加http://localhost:8080作为域名
             '**.css' : 'http://localhost:8080'
-        },
-        path : [
-            {
-                reg : /^\/pages\/(.*\.html)/i,
-                release : '/static/$1'
-            },
-            {
-                reg : /^\/dist\/css\/(.*\.css|.*\.styl)/i,
-                release : '/themes/css/$1'
-            },
-            {
-                reg : /^\/dist\/js\/(.*\.js)/i,
-                release : '/themes/js/$1'
-            },
-            {
-                reg : /^\/dist\/data\/(.*\.js)/i,
-                release : '/static/data/$1'
-            }
-        ]
+        }
     }
 });
