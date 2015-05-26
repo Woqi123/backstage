343813227bff1ffeb43b61b75d845cbd0de76a25
chenhao <ch@qian360.com>
2015-05-26 15:15:41 +0800

    结构修改

--- themes/js/common.js
+++ themes/js/common.js
@@ -1,11 +1,10 @@
-//顶部条的问候
-angular.module('ngApp', []).controller('navTopGreet', function ($scope) {
-    $scope.hello = '晚上好';
-});
 $(function () {
 
     //侧栏收缩
     $('#side-menu').metisMenu();
+    //$('#side-menu li').on('click',function(){
+    //    $(this).addClass('active').siblings('li').removeClass('active');
+    //});
 
     //展示区域iframe高度计算
     $("#frameWrapper").load(function () {
@@ -16,4 +15,29 @@ $(function () {
     //title提示框美化
     $('[data-toggle="tooltip"]').tooltip();
 
+    $(window).bind("load resize", function() {
+        var topOffset = 50,
+            width = (this.window.innerWidth > 0) ? this.window.innerWidth : this.screen.width;
+        if (width < 768) {
+            $('div.navbar-collapse').addClass('collapse');
+            topOffset = 100;
+        } else {
+            $('div.navbar-collapse').removeClass('collapse');
+        }
+
+        var height = ((this.window.innerHeight > 0) ? this.window.innerHeight : this.screen.height) - 1;
+        height = height - topOffset;
+        if (height < 1) height = 1;
+        if (height > topOffset) {
+            $("#page-wrapper").css("min-height", (height) + "px");
+        }
+    });
+
+    var url = window.location;
+    var element = $('ul.nav a').filter(function() {
+        return this.href == url || url.href.indexOf(this.href) == 0;
+    }).addClass('active').parent().parent().addClass('in').parent();
+    if (element.is('li')) {
+        element.addClass('active');
+    }
 });
