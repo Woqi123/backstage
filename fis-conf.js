8e282cff842e9b560b3c2635449864245313eda8
xjchenhao <wy.chenhao@qq.com>
2016-02-18 17:28:31 +0800

    第三方库为了区分管理依然使用bower

--- fis-conf.js
+++ fis-conf.js
@@ -1,1 +1,1 @@
-//fis.set('project.ignore', ['output/**', 'fis-conf.js']);
+fis.set('project.ignore', ['node_modules/**', 'fis-conf.js','gulpfile.js','package.json','README.md','bower.json','.gitignore','.bowerrc']);
